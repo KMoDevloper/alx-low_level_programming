6b43a58a3c9f06e9d1404869472a5de8762a52a8
KMoDevloper <mouaouiyakawtar@gmail.com>
2023-10-12 14:17:49 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9 followed by a new line
+ */
+
+void print_numbers(void)
+{
+	char digit = '0';
+
+	while (digit <= '9')
+	{
+		_putchar(digit);
+		digit++;
+	}
+	_putchar('\n');
+}
