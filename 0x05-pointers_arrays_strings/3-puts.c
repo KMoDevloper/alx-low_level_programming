6963389da5a6fb1c00e655c57f8ea95147607b60
KMoDevloper <mouaouiyakawtar@gmail.com>
2023-10-16 15:01:01 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line.
+ * @str: Pointer to the string.
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
