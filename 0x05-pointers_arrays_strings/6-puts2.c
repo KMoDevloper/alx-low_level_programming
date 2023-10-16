d616badbe128e2cdf976c1abf92258447245e566
KMoDevloper <mouaouiyakawtar@gmail.com>
2023-10-16 16:36:53 -0700

    c6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,17 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
+	int j = 0;
 
-	while (str[i] != '\0')
+	while (str[j] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		j++;
 	}
 
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
