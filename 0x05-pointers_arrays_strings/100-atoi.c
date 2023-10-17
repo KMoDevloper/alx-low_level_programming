3effa83e4aeca295830de268d17e977f02570b3f
KMoDevloper <mouaouiyakawtar@gmail.com>
2023-10-16 17:16:59 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,35 +3,33 @@
 /**
  * _atoi - Converts a string to an integer.
  * @s: Pointer to the string to be converted.
- * Return: The converted integer.
+ * Return: A integer.
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+	int c = 1;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	while (s[i] == ' ' || (s[i] == '-' || s[i] == '+'))
+	while (s[c])
 	{
-		if (s[i] == '-')
+		if (s[c] == 45)
 		{
-			sign *= -1;
+			min *= -1;
 		}
-		i++;
-	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+		while (s[c] >= 48 && s[c] <= 57)
 		{
-			if (sign == 1)
-				return (INT_MAX);
-			else
-				return (INT_MIN);
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++
 		}
-		result = result * 10 + (s[i] - '0');
-		i++;
+		if (isi == 1)
+		{
+			break;
+		}
+		c++;
 	}
-
-	return (result * sign);
+	ni *= min;
+	return (ni);
 }
