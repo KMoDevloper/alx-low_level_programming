a8d960f3783d7a6ef5f2eaa6608bc594a908c36f
KMoDevloper <mouaouiyakawtar@gmail.com>
2023-10-19 16:32:01 -0700

    print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,16 +6,30 @@
  */
 void print_number(int n)
 {
+	int divisor = 1;
+	int is_negative = 0;
+
 	if (n < 0)
 	{
 		_putchar('-');
+		is_negative = 1;
 		n = -n;
 	}
 
-	if (n / 10 != 0)
+	while (n / divisor >= 10)
 	{
-		print_number(n / 10);
+		divisor *= 10;
+	}
+	while (divisor != 0)
+	{
+		int digit = n / divisor;
+		_putchar(digit + '0');
+		n %= divisor;
+		divisor /= 10;
 	}
 
-	_putchar(n % 10 + '0');
+	if (is_negative)
+	{
+		_putchar('-');
+	}
 }
