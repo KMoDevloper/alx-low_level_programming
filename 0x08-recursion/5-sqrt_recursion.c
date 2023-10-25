324f6bff22ccea1a3921623617e9824626a2e387
KMoDevloper <mouaouiyakawtar@gmail.com>
2023-10-25 14:09:30 -0700

    sqrt_recursive

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,19 @@
 #include "main.h"
 /**
- * sqrt_recursive - Calculates the square root of a number recursively.
+ * _sqrt_recursion - Calculates the square root of number recursively.
+ * @n: The number to find the square root of.
+ *
+ * Return: The square root of n, or -1
+ * if n does not have a natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_recursive(n, 1));
+}
+
+/**
+ * sqrt_recursive - helper function to calculate.
+ * the square root of a number recursively.
  * @n: The number to find the square root of.
  * @guess: The current guess for the square root.
  *
