e59e70357170189786547f85484fff612a732850
KMoDevloper <mouaouiyakawtar@gmail.com>
2023-11-01 13:53:58 -0700

    intmain

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 of successful, 1 if there are not enough arguments
+ */
+intmain(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (atgc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+
+	printf("d\n", result);
+
+	return (0);
+}
