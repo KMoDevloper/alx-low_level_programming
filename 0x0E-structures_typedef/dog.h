712a9961781c98aa459d3704f6a7dd52e80cf6ab
KMoDevloper <mouaouiyakawtar@gmail.com>
2023-11-06 14:10:57 -0800

    Dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,12 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
+/* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
