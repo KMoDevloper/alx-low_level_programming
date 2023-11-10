04161ff0e86c548e5dbd2c8c5569dda83ed7658e
KMoDevloper <mouaouiyakawtar@gmail.com>
2023-11-09 16:14:45 -0800

    3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,44 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - Prints anything based on the format string.
+ * @format: A list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *separator = "";
+	char *str;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", separator, str);
+				break;
+		}
+		separator = ", ";
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,28 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+/**
+ * struct print_format - Struct to hold a format
+ * specifier and its corresponding printer function
+ * @format: The format specifier (e.g., "c", "i", "f", "s")
+ * @printer: Pointer to the corresponding printer function
+ */
+typedef struct print_format
+{
+	char *format;
+	void (*printer)(va_list);
+} print_format_t;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
 
 
 #endif
