124dae311081f9ce4125e3d24cd6dbeb29ecded7
KMoDevloper <mouaouiyakawtar@gmail.com>
2023-11-17 17:44:41 -0800

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -8,6 +8,7 @@ extern printf
 main:
 sub rsp, 8 ; Align the stack (must be a multiple of 16 before calling printf)
 mov rdi, hello
+mov rax, 0 ; printf uses the xmm0 rgister for floating-printf arguments, clear it
 call printf
 add rsp, 8 ; Clean up the stack
 ret
