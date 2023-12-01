ab8a2307058986076aed4938109698df83cb987e
KMoDevloper <mouaouiyakawtar@gmail.com>
2023-12-01 12:18:32 -0800

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * copies the content of a file to another file.
+ * @argc: The number of arguments.
+ * @argv: An array of strings containing the arguments.
+ * Return: 0 on success, or an exit code on failure.
+ */
+int main(int argc, char*argv[])
+{
+	int fd_from, fd_to, read_result, write_result;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: %s file_from_to\n", argv[0]);
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from);
+		exit(99);
+	}
+
+	while ((read_result = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		write_result = write(fd_to, buffer, read_result);
+		if (write_result == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(2, "Errof: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
