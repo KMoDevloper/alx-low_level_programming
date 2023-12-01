621318b1cbb85b2dc2f6dbd7fd9ec74b1fe0df78
KMoDevloper <mouaouiyakawtar@gmail.com>
2023-12-01 10:42:53 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR);
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR);
 	if (file_descriptor ==  -1)
 		return (-1);
 
